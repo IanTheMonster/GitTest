6585f3cecec2227f7b10325f3ca0719000ca7973
Ian Mac <827548606@qq.com>
2024-04-18 14:47:09 +0800

    常见面试题

---
+++ Debounce/防抖.js
@@ -0,0 +1,23 @@
+function debounce(fn,delay) {
+    let t = null ; //定义定时器
+    return function f(){
+        if ( t != null ){
+            clearTimeout(t);
+        }
+        t = setTimeout(()=>{
+            fn.call(this);
+        },delay);
+    }
+}
+function throttle(fn,delay){
+    let  t = true; //默认存在
+    return function f(){
+        if(t){
+            fn.call(this);//执行操作
+            setTimeout(()=>{
+                t = true;
+            },delay);
+            t = false;
+        }
+    }
+}
